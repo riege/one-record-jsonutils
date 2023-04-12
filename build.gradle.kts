76ddbd319fc4d64d77e3d760438a18c3fbbde232
Martin Skopp <skopp@riege.com>
2023-04-12 17:23:57 +0200

    Add GitHub workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
@@ -5,26 +7,40 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group "com.riege"
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+group = "com.riege"
+
+/*
+ * Gets the version name from the latest Git tag, omit leading "v" - yeah!
+ * Note: a plugin way to do this would be via https://plugins.gradle.org/plugin/net.nemerosa.versioning
+ */
+val gitVersion: String = ByteArrayOutputStream().use { outputStream ->
+    project.exec {
+        commandLine("git", "describe", "--tags", "--always")
+        standardOutput = outputStream
+    }
+    outputStream.toString().trim().replace("\"", "")
 }
+if (version == "unspecified") {
+    version = gitVersion
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
-    // api("com.fasterxml.jackson.core:jackson-databind:2.14.2")
     api("com.fasterxml.jackson.core:jackson-annotations:2.14.2")
-    // Java 8 date/time type `java.time.OffsetDateTime` not supported by default
-    // so we need to add com.fasterxml.jackson.datatype:jackson-datatype-jsr310:
+    // Java 8 date/time type java.time.OffsetDateTime not supported by default,
+    // so we need to add com.fasterxml.jackson.datatype:jackson-datatype-jsr310
     api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.2")
 
     // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.named<Test>("test") {
